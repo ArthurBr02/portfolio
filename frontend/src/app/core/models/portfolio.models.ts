b905fdd534e0b555985abbb16280abb6df8c33fb
Arthur <arthurbratigny@gmail.com>
2026-05-26 15:55:27 +0200

    UF

--- frontend/src/app/core/models/portfolio.models.ts
+++
@@ -1,5 +0,0 @@
-export * from './skill.model';
-export * from './project.model';
-export * from './experience.model';
-export * from './education.model';
-export * from './portfolio-data.model';
--- frontend/src/app/core/models/project.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Project {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    technologies: string[];
-    liveUrl?: string;
-    githubUrl?: string;
-}
